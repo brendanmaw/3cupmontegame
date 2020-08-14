ebf8ea24178e854bc14e09f7be560df78cdf7212
brendanmaw <69124355+brendanmaw@users.noreply.github.com>
2020-08-14 09:40:07 -0600

    Add files via upload

---
+++ 3CupMonteGame.py
@@ -0,0 +1,37 @@
+from random import shuffle
+
+# Initial List
+mylist = ['', 'O', '']
+
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return(mylist)
+
+
+def player_guess():
+
+    guess = ''
+
+    while guess not in ['0', '2', '1']:
+        guess = input(
+            'Pick a number between 0-2, if you find the ball, you win! ')
+    return int(guess)
+
+
+def check_guess(mylist, guess):
+    if mylist[guess] == 'O':
+        print('Correct')
+    else:
+        print('Wrong guess!')
+        print(mylist)
+
+
+# Shuffle List
+shuffled_list = shuffle_list(mylist)
+
+# User Guess
+guess = player_guess()
+
+# Check Guess
+check_guess(shuffled_list, guess)
